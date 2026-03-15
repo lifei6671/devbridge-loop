33ec904361f03939c3c38e0b4eaacdff770040aa
lifei6671 <lifei6671@gmail.com>
2026-03-15 17:56:01 +0800

    bugfix

---
+++ agent-core/pkg/events/consts.go
@@ -0,0 +1,13 @@
+package events
+
+const (
+	EventTrace = "trace"
+	EventDebug = "debug"
+	EventInfo  = "info"
+	EventWarn  = "warn"
+	EventError = "error"
+)
+
+const (
+	EventCode = "SESSION_RECONNECT_REQUESTED"
+)
